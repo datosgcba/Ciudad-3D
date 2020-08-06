3301e5b26f434a975d1c6e167b112ddc7f158637
Luciano Kavcic <lek.correo@gmail.com>
2020-08-06 19:38:43 -0300

    se acomodó map.js para unificar criterios

--- src/components/Mapa/Mapa.js
+++ src/components/Mapa/Mapa.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { renderToString } from "react-dom/server";
 import MapaInteractivoGL from "../../utils/MapaInteractivoGL";
@@ -7,37 +7,21 @@ import Buscador from "../Buscador/Buscador";
 import { updateMap, initMap } from "../../store/actions";
 import imgCapaBasePrincipal from "../../img/capabase_1.png";
 import imgCapaBaseSecundaria from "../../img/capabase_2.png";
-import LogoutButton from '../LogoutButton/LogoutButton'
-
+import LogoutButton from '../LogoutButton/LogoutButton';
+import { useSelector, useDispatch } from 'react-redux'
 import "./styles.css";
 
-const mapStateToProps = state => {
-  return {
-    map: state.map.mapaGL
-  };
-};
-
-function mapDispatchToProps(dispatch) {
-  return {
-    updateMapAction: map => dispatch(updateMap(map)),
-    initMapAction: () => dispatch(initMap())
-  };
-}
-
-class ConnectedMap extends Component {
-  state = {
-    capabasePrincipal: true
-  };
-  
-  toogleBaseLayer = () => {
-    const { map } = this.props;   
-    const { capabasePrincipal } = this.state;
+const Mapa = (props) => {
+  const map = useSelector(state => state.map.mapaGL);
+  const dispatch = useDispatch()
+  const [capabasePrincipal, setCapabasePrincipal] = useState(true)
+  const { logged, updateMapAction, initMapAction } = props;
+  const toogleBaseLayer = () => {
     map.toggleBaseLayer();
-    this.setState({ capabasePrincipal: !capabasePrincipal });
+    setCapabasePrincipal(!capabasePrincipal);
   };
 
-  onFeatureClick = (lngLat, feature, layerId, layerName) => {
-    const { map } = this.props;
+  const onFeatureClick = (lngLat, feature) => {
     map
       .getFeatureProps(feature.properties.Id)
       .then(res => res.json())
@@ -50,59 +34,47 @@ class ConnectedMap extends Component {
       });
   };
 
-  componentDidMount() {
-
-  }
-  componentDidUpdate(){
-    const { logged, data, map, updateMapAction, initMapAction } = this.props;
-
-    //chanchada: resolver mejor esto
+  useEffect(() => {
     if (!map) {
       setTimeout(() => {
         const instanciaMap = new MapaInteractivoGL({
-          onFeatureClick: this.onFeatureClick
+          onFeatureClick
         });
 
         //dispatch de la accion para guardar la instancia en el store
-        updateMapAction(instanciaMap);
+        dispatch(updateMap(instanciaMap))
 
         //agrego las capas prendidas por default
-        initMapAction();
+        dispatch(initMap())
       }, 500);
     }
-  }
+  }, [map, dispatch])
 
-  render() {
-    
-    const { capabasePrincipal } = this.state;
-
-    return (
-      <div id="map">
-        <div className="topMenu">
-          <Buscador />
-          {this.props.logged ? <LogoutButton/> : null}
-
-        </div>
-        <div className="bottomMenu" onClick={this.toogleBaseLayer}>
-          <div
-            className="minimap-layer"
-            style={{
-              backgroundImage: !capabasePrincipal
-                ? `url(${imgCapaBasePrincipal})`
-                : `url(${imgCapaBaseSecundaria})`
-            }}
-          >
-            <div className="minimap-title-container">
-              <div className="minimap-title">
-                {!capabasePrincipal ? "Modo Oscuro" : "Modo Claro"}
-              </div>
+  return (
+    <div id="map">
+      <div className="topMenu">
+        <Buscador />
+        {logged ? <LogoutButton /> : null}
+      </div>
+      <div className="bottomMenu" onClick={toogleBaseLayer}>
+        <div
+          className="minimap-layer"
+          style={{
+            backgroundImage: !capabasePrincipal
+              ? `url(${imgCapaBasePrincipal})`
+              : `url(${imgCapaBaseSecundaria})`
+          }}
+        >
+          <div className="minimap-title-container">
+            <div className="minimap-title">
+              {!capabasePrincipal ? "Modo Oscuro" : "Modo Claro"}
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
 
-const Mapa = connect(mapStateToProps, mapDispatchToProps)(ConnectedMap);
+
+}
 export default Mapa;

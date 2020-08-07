fc4f6b2d23e6079701c0b88b5f25a5a0bb5c3e1d
LuchoK90 <lek.correo@gmail.com>
2020-08-07 16:52:54 -0300

    [1469] Se le da dinamismo al subsection

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -1,21 +1,23 @@
-import React from "react"
-import { Drawer } from "@material-ui/core"
+import React from 'react'
+import { Drawer } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 import Explorer from './SubSection/Explorer'
 
 const Section = () => {
-    const isShow = useSelector(state => state.map.sectionOpen)
-    return (
-        /*
+  const isShow = useSelector((state) => state.map.sectionOpen)
+  return (
+
         <Drawer
-            open={isShow}
+      variant="persistent"
+      open={isShow}
+      style={{ position: 'absolute', paddingLeft: 100 }}
         >
-        */
-            <Explorer title="Explorar" />
-        /*
+    
+    <Explorer title="Explorar" />
+
         </Drawer>
-        */
-    )
+     
+  )
 }
 
 export default Section

0eb0849b591fa289e5d1d5e423117d22d66e7182
Lopez Gonzalo <lopezgonzalo.sm89@gmail.com>
2020-08-08 16:19:17 -0300

    [1469] Componente Visualize

--- src/components/Grupo/Grupo.js
+++ src/components/Grupo/Grupo.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import Divider from '@material-ui/core/Divider'
-import HelpIcon from '@material-ui/icons/Help'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Tooltip from '@material-ui/core/Tooltip'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
-import { Checkbox, ListItemIcon } from '@material-ui/core'
+import { Checkbox } from '@material-ui/core'
 import './styles.css'
-import CloudDownloadIcon from '@material-ui/icons/CloudDownload'
-import IconButton from '@material-ui/core/Button'
 import { toggleLayer } from '../../store/actions'
 
 const Grupo = (props) => {
@@ -17,8 +13,6 @@ const Grupo = (props) => {
   props.layers.forEach((layer) => {
     initialState[layer.id] = layer.enabled ? layer.enabled : false
   })
-
-  const { logged } = props
 
   const [layers, setLayers] = useState(initialState)
   const [isChecked, setIsChecked] = useState(initialState)
@@ -31,51 +25,9 @@ const Grupo = (props) => {
     setLayers({ ...layers, [layer.id]: checked })
   }
 
-  const handlePrivacy = () => {
-    if (!logged) {
-      return (
-        props.layers.filter((layer) => !layer.private)
-          .map((layer) => (
-            <ListItem key={layer.id} className="item-layer">
-              <FormControlLabel
-                className="item-layer-label"
-                control={(
-                  <Checkbox
-                    checked={layers[layer.id]}
-                    onChange={layerChangeHandler(layer)}
-                    name={layer.id}
-                    color="primary"
-                  />
-                )}
-                label={layer.title}
-              />
-
-              {isChecked[layer.id]
-
-                && (
-                <img
-                  hspace="7"
-                  src={layer.icon}
-                  alt="icono"
-                  height="20"
-                  width="20"
-                />
-                )}
-
-              {isChecked[layer.id] && layer.link
-              && (
-              <IconButton color="primary" href={layer.link} target="_blank">
-                {' '}
-                <CloudDownloadIcon />
-                {' '}
-              </IconButton>
-              )}
-            </ListItem>
-          ))
-      )
-    }
-    return (
-      props.layers.map((layer) => (
+  const handlePrivacy = () => (
+    props.layers.filter((layer) => !layer.private)
+      .map((layer) => (
         <ListItem key={layer.id} className="item-layer">
           <FormControlLabel
             className="item-layer-label"
@@ -84,59 +36,32 @@ const Grupo = (props) => {
                 checked={layers[layer.id]}
                 onChange={layerChangeHandler(layer)}
                 name={layer.id}
-                color="primary"
+                color="text"
               />
-                )}
-            label={layer.title}
-          />
-
-          {isChecked[layer.id]
-
-                && (
-                <img
-                  hspace="7"
-                  src={layer.icon}
-                  alt="icono"
-                  height="20"
-                  width="20"
-                />
-                )}
-
-          {isChecked[layer.id] && layer.link
-              && (
-              <IconButton color="primary" href={layer.link} target="_blank">
-                {' '}
-                <CloudDownloadIcon />
-                {' '}
-              </IconButton>
               )}
+          />
+          <img
+            hspace="5"
+            src={layer.icon}
+            alt="icono"
+            height="20"
+            width="20"
+          />
+          <h5>{layer.title}</h5>
         </ListItem>
       ))
-
-    )
-  }
+  )
 
   return (
     <>
       <div className="contenedor-layers">
         <h4 className="titulo-principal">
           {props.title}
-          <Tooltip
-            title={props.help}
-            placement="right-start"
-            arrow
-            className="layer-tooltip"
-          >
-            <ListItemIcon>
-              <HelpIcon className="icono-help" />
-            </ListItemIcon>
-          </Tooltip>
           <Divider />
         </h4>
 
         <List dense className="lista-layers">
           {handlePrivacy()}
-
         </List>
       </div>
     </>
--- src/components/Sections/SubSection/Visualize/index.js
+++ src/components/Sections/SubSection/Visualize/index.js
@@ -1,63 +1,38 @@
-import React, { Component } from "react";
-import Container from "@material-ui/core/Container";
-import { connect } from "react-redux";
-import config from "../../../../config";
-import Grupo from "../../../Grupo/Grupo"
-import { updateMap } from "../../../../store/actions";
-import "./styles.js";
-import Scrollbar from "react-smooth-scrollbar";
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import config from '../../../../config'
+import Grupo from '../../../Grupo/Grupo'
+import './styles.js'
+import Scrollbar from 'react-smooth-scrollbar'
 
-class Visualize extends Component {
-    render() {
-        const handlePrivacy = () => {
-            return (config.grupos.map((g, index) => (
-                <Grupo
-                    key={index}
-                    color={g.color}
-                    title={g.title}
-                    help={g.help}
-                    layers={g.layers}
-                />
-            )))
-        }
-        const grupos = config.grupos.map((g, index) => (
-            <Grupo
-                key={index}
-                color={g.color}
-                title={g.title}
-                help={g.help}
-                layers={g.layers}
-            />
-        ));
+const Visualize = () => {
+  const handlePrivacy = () => (config.grupos.map((g, index) => (
+    <Grupo
+      key={index}
+      color={g.color}
+      title={g.title}
+      help={g.help}
+      layers={g.layers}
+    />
+  )))
 
-        return (
-            <Scrollbar style={{ marginBottom: '5px' }}>
-                <Container
-                    maxWidth="sm"
-                    className="contenedor"
-                    style={{ paddingLeft: "20px", paddingRight: "26px", marginBottom: '15px',
-                    marginLeft: 100, }}>
-                    {/* {grupos} */}
-                    {handlePrivacy()}
-                </Container>
-            </Scrollbar>
-        );
-    }
+  return (
+    <Scrollbar style={{ marginBottom: '5px' }}>
+      <Container
+        maxWidth="sm"
+        className="contenedor"
+        style={{
+          paddingLeft: '20px',
+          paddingRight: '26px',
+          marginBottom: '15px',
+          marginLeft: 100,
+        }}
+      >
+        {/* {grupos} */}
+        {handlePrivacy()}
+      </Container>
+    </Scrollbar>
+  )
 }
 
-function mapDispatchToProps(dispatch) {
-    return {
-        updateMapInstance: map => dispatch(updateMap(map))
-    };
-}
-
-const mapStateToProps = state => {
-    return {}
-};
-
-const PanelLateral = connect(
-    mapDispatchToProps,
-    mapStateToProps
-)(Visualize);
-
-export default PanelLateral;
+export default Visualize

94f5a8487a07efa463547f767fed7b2e68602bcb
Sebastian Lopez <sebastian.lopez@leniolabs.com>
2020-08-12 11:05:26 -0300

    [1471] import de fuentes utilizando react-font-loader

--- src/index.js
+++ src/index.js
@@ -3,21 +3,27 @@ import { render } from 'react-dom'
 
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/styles'
+import theme from 'theme'
 
 import { Provider } from 'react-redux'
+
+import ReactFontLoader from 'react-font-loader'
 
 import store from 'state'
 
 import App from './App'
-import theme from './theme'
 
 import * as serviceWorker from './serviceWorker'
 
+// const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// const store = createStore(rootReducer, composeEnhances(applyMiddleware(thunk)))
 render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
+      <ReactFontLoader fonts={[{ name: 'Nunito' }]} />
       <App />
     </ThemeProvider>
   </Provider>,

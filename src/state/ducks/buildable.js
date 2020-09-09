6332b751c9248562cee3edc7f2f0cb000a18a2c0
LuchoK90 <lek.correo@gmail.com>
2020-09-09 12:38:13 -0300

    [1529] Se unifica en una API Edificabilidad

--- src/state/ducks/buildable.js
+++ src/state/ducks/buildable.js
@@ -1,11 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
-import { getBoundariesParcelsApi } from 'utils/apiConfig'
+import { getBuildableApi } from 'utils/apiConfig'
 
 const clickOnParcel = createAsyncThunk(
   'buildable/clickOnParcel',
   async (coord) => {
-    const urlApi = getBoundariesParcelsApi(coord)
+    const urlApi = getBuildableApi(coord)
     const response = await fetch(urlApi)
     const data = (await response.json())
     // TODO: traer sólo lo necesario
--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -2,14 +2,8 @@ const getParcelApi = ({ lng, lat }) => `https://epok.buenosaires.gob.ar/catastro
 
 const getGeometricalApi = (smp) => `https://epok.buenosaires.gob.ar/catastro/geometria/?smp=${smp}`
 
-const getBuildableApi = ({ lng, lat, smp }) => `https://epok.buenosaires.gob.ar/catastro/seccion_edificabilidad/?smp=${smp}&lng=${lng}&lat=${lat}`
-
-const getBoundariesParcelsApi = ({ lng, lat }) => `https://epok.buenosaires.gob.ar/catastro/consultar_parcelas_linderas/?lng=${lng}&lat=${lat}`
-
-const getCapitalGainApi = ({
-  lng, lat, area_edificar, smp
-}) => `https://epok.buenosaires.gob.ar/catastro/calcular_plusvalia/?lng=${lng}&lat=${lat}&smp=${smp}&ae=${area_edificar}`
+const getBuildableApi = (smp) => `https://epok.buenosaires.gob.ar/catastro/seccion_edificabilidad/?smp=${smp}`
 
 export {
-  getParcelApi, getGeometricalApi, getBuildableApi, getBoundariesParcelsApi, getCapitalGainApi
+  getParcelApi, getGeometricalApi, getBuildableApi
 }
